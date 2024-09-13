4b38b3f2b8b16dc17abfabfc0ec7c928ee08416a
Aliverdiyev <aliverdiyev00@gmail.com>
2024-09-13 15:16:30 +0400

    calculate points

--- src/Main.java
+++ src/Main.java
@@ -8,14 +8,38 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+
         System.out.println("Enter number of credits:");
         String credits = sc.nextLine();
+
         System.out.println("Enter grade:");
         String grade = sc.nextLine();
 
-        System.out.println(credits);
-        System.out.println(grade);
+        Integer gradeValue = 0;
+        if (grade.equalsIgnoreCase("A")) {
+            gradeValue = 4;
+        } else if (grade.equalsIgnoreCase("B")) {
+            gradeValue = 3;
+        } else if (grade.equalsIgnoreCase("C")) {
+            gradeValue = 2;
+        } else if (grade.equalsIgnoreCase("D")) {
+            gradeValue = 1;
+        } else if (grade.equalsIgnoreCase("F")) {
+            gradeValue = 0;
+        } else {
+            System.out.println("Invalid grade");
+        }
+
+        Integer points = gradeValue * Integer.parseInt(credits);
+        Integer gpa = points / Integer.parseInt(credits);
+
+        System.out.println("Credits: " + credits);
+        System.out.println("Grade: " + grade);
+        System.out.println("Points: " + points);
+        System.out.println("GPA: " + gpa);
 
         sc.close();
+
+
     }
 }
