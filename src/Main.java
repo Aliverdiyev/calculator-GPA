c26c3a55144347f60dbf988e50d71344c7a93521
Aliverdiyev <aliverdiyev00@gmail.com>
2024-09-13 17:43:56 +0400

    Edits in comment

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 /*
  total point / total credits
  points for a class = grade value * credits
- A = 5, B = 3.....
+ A = 4, B = 3.....
 */
 public class Main {
     public static void main(String[] args) {
