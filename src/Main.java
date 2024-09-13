f39eec603edb5f069663f8bc2fa18acc01405335
Aliverdiyev <aliverdiyev00@gmail.com>
2024-09-13 14:46:50 +0400

    scanner input

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,21 @@
-public class Main {
+import java.util.Scanner;
 
+/*
+ total point / total credits
+ points for a class = grade value * credits
+ A = 5, B = 3.....
+*/
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter number of credits:");
+        String credits = sc.nextLine();
+        System.out.println("Enter grade:");
+        String grade = sc.nextLine();
+
+        System.out.println(credits);
+        System.out.println(grade);
+
+        sc.close();
+    }
 }
